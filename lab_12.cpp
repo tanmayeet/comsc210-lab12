fe12b8af443ee547c95a59bd41146e1a1f2b2927
Tanmayee Chalamalasetti <tanmayeesc@MacBookPro.attlocal.net>
2025-09-21 20:10:25 -0700

    Had to restart as file was corrupted

---
+++ lab_12.cpp
@@ -0,0 +1,27 @@
+// COMSC210 | Lab 12 | Tanmayee Chalamalasetti
+// IDE Used: VS Code
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Song {
+  std::string name;
+  std::string genre;
+  std::string artist;
+  int weekNumber;
+}
+
+    void addSongs() {
+  Song s;
+}
+
+void displaySongs() {}
+
+int main() {
+  std::array<Song, 52>;
+
+  return 0;
+}
