56dba12c77767691aa1332b0744acd5098a5be77
Tanmayee Chalamalasetti <tanmayeesc@MacBookPro.attlocal.net>
2025-09-21 20:35:32 -0700

    Push 2

--- lab_12.cpp
+++ lab_12.cpp
@@ -14,16 +14,43 @@ struct Song {
   int weekNumber;
 }
 
-void readSongs() {
-  Song s;
+void readSongs(array<Song, 52>& playlist, const string& filename) {
+  ifstream file(filename);
+  int num = 0;
+  while (file && num < playlist.size()) {
+    file >> playlist[num].name >> playlist[num].genre >> playlist[num].artist >>
+        playlist[num].weekNumber;
+    num++;
+  }
+  file.close();
 }
 
-void displaySongs() {}
+void displaySongs(const array<Song, 52>& playlist) {
+  cout << "Weekly Playlist!\n";
+  for (const auto& song : playlist) {
+    if (!song.name.empty()) {
+      cout << "Week" << song.weekNumber << ": " << song.name << "  by "
+           << song.artist << " [" << song.genre << "]\n";
+    }
+  }
+}
 
-void analyzePlaylist(const array<Song, 52>& playlist) {}
+void analyzePlaylist(const array<Song, 52>& playlist) {
+  cout << "Playlist Wrapped:\n";
+  cout << "Total capacity: " << playlist.size() << endl;
+  cout << "Is playlist empty? ";
+  if (playlist.empty()) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
 
 int main() {
   array<Song, 52> playlist{};
+  readSongs(playlist, "songs.txt");
+  displaySongs(playlist);
+  analyzePlaylist(playlist);
 
   return 0;
 }
